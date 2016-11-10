d38d2041e221fd3a0d9726bccd3f5a0888c00108
Yin zj <Yin zj@gmail.com>
2016-11-10 20:10:53 +0800

    在Actionbar添加一个返回按钮

--- app/src/main/java/googleplay/itheima/cn/googleplay/MainActivity.java
+++ app/src/main/java/googleplay/itheima/cn/googleplay/MainActivity.java
@@ -1,15 +1,17 @@
 package googleplay.itheima.cn.googleplay;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.SearchView.OnQueryTextListener;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.EditText;
+import android.view.View;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnQueryTextListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +19,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void onclick(View v){
+        Intent intent = new Intent(getApplicationContext(), DetailActivity.class);
+        startActivity(intent);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main_activity_actions,menu);
+
+//        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+//        searchView.setOnQueryTextListener(this); //搜索的监听
         return super.onCreateOptionsMenu(menu);
     }
 
@@ -32,5 +41,19 @@ public class MainActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    //当搜索提交时
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+//        Toast.makeText(getApplicationContext(),query,Toast.LENGTH_SHORT).show();
+        return true;
+    }
+
+    //当搜索发生时
+    @Override
+    public boolean onQueryTextChange(String newText) {
+//        Toast.makeText(getApplicationContext(),"搜索：" + newText,Toast.LENGTH_SHORT).show();
+        return true ;
     }
 }
