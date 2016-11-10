e894e033dbc1b26f568020cc19f817af3ca0b3ff
Yin zj <Yin zj@gmail.com>
2016-11-10 21:01:05 +0800

    在Actionbar添加标签栏

--- app/src/main/java/googleplay/itheima/cn/googleplay/MainActivity.java
+++ app/src/main/java/googleplay/itheima/cn/googleplay/MainActivity.java
@@ -2,6 +2,8 @@ package googleplay.itheima.cn.googleplay;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.SearchView.OnQueryTextListener;
@@ -17,8 +19,36 @@ public class MainActivity extends AppCompatActivity implements OnQueryTextListen
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        ActionBar.Tab tab1 = actionBar.newTab().setText("标签一").setTabListener(new MyTabListener());
+        actionBar.addTab(tab1);
+        ActionBar.Tab tab2 = actionBar.newTab().setText("标签二").setTabListener(new MyTabListener());
+        actionBar.addTab(tab2);
+        ActionBar.Tab tab3 = actionBar.newTab().setText("标签三").setTabListener(new MyTabListener());
+        actionBar.addTab(tab3);
+        ActionBar.Tab tab4 = actionBar.newTab().setText("标签四").setTabListener(new MyTabListener());
+        actionBar.addTab(tab4);
     }
 
+    private class MyTabListener implements ActionBar.TabListener{
+
+        @Override
+        public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+        }
+
+        @Override
+        public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+        }
+
+        @Override
+        public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+        }
+    }
     public void onclick(View v){
         Intent intent = new Intent(getApplicationContext(), DetailActivity.class);
         startActivity(intent);
