3391ec2d1c244ec7b7e3f81635cc1a7a6fb12542
Yin zj <Yin zj@gmail.com>
2016-11-10 17:52:34 +0800

    在Actionbar添加一个搜索按钮

--- app/src/main/java/googleplay/itheima/cn/googleplay/MainActivity.java
+++ app/src/main/java/googleplay/itheima/cn/googleplay/MainActivity.java
@@ -1,7 +1,13 @@
 package googleplay.itheima.cn.googleplay;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_actions,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_search :
+                Toast.makeText(getApplicationContext(),"搜索",Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
